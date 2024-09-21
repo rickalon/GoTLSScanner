c532a95e212a05f1f8813cbd656e62cfd3f3e20a
Ricardo <ricardoaloof@gmail.com>
2024-09-21 23:12:40 +0200

    Util json error msg added

--- util/util.go
+++ util/util.go
@@ -1,9 +1,32 @@
 package util
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Message struct {
+	Msg string `json:"message"`
+}
+
+type ErrorMessage struct {
+	Msg string `json:"error"`
+}
 
 func MsgToJson(w http.ResponseWriter, msg string) {
-	w.Header().Add("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(msg))
+	json.NewEncoder(w).Encode(&Message{Msg: msg})
+}
+
+func ErrorToJson(w http.ResponseWriter, statusCode int, err error) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(&ErrorMessage{Msg: err.Error()})
+}
+
+func ErrorStringToJson(w http.ResponseWriter, statusCode int, err string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(&ErrorMessage{Msg: err})
 }
